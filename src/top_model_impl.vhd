7b5670b512048e9847d0354f3cfb3ee8c72c28f8
geekskick <geeks_kick@yahoo.co.uk>
2019-11-12 21:44:18 +0000

    Made a generate statement in the top model

--- src/top_model_impl.vhd
+++ src/top_model_impl.vhd
@@ -5,20 +5,23 @@ use work.tutorial_package.all;
 architecture structural of top_model is
 
     constant counter_width : positive := 4;
+    type counter_array is array (0 to num_lights-1) of std_logic_vector(counter_width-1 downto 0);
+    signal cout : counter_array := (others => (others =>'0'));
 
-    signal c1_out : std_logic_vector(counter_width-1 downto 0) := (others => '0');
 begin
-    c1: counter generic map( 
-        data_width => counter_width
-    )
-    port map(
-        clk        => clk,
-        en         => en,
-        rst        => rst,
-        q          => c1_out
-    );
+    counters: for i in 0 to num_lights-1 generate
+        cx : counter generic map(
+            data_width => counter_width
+        )
+        port map(
+            clk        => clk,
+            en         => en,
+            rst        => rst,
+            q          => cout(i)
+        );
+    end generate;
 
-    lights(0) <= c1_out(0);
+    lights(0) <= cout(0)(0);
 end architecture;
 
 
